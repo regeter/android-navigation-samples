7f8016d9a1948c880ef093ff63d08ddbd161174a
regeter <2320305+regeter@users.noreply.github.com>
2024-10-14 16:55:47 -0700

    Route Testing.

--- navigation-sample/app/src/main/java/com/example/navigationapidemo/NavFragmentActivity.kt
+++ navigation-sample/app/src/main/java/com/example/navigationapidemo/NavFragmentActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.navigationapidemo
 
 import android.annotation.SuppressLint
@@ -26,7 +10,6 @@ import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -96,8 +79,9 @@ class NavFragmentActivity : AppCompatActivity() {
     // Define button configurations
     val buttonConfigs = listOf(
       ButtonConfig("Truck: Harrisburg PA") { customNavigate(Waypoint.builder().setLatLng(40.305301, -76.888941).build(), routesApiTruckToken) },
-      ButtonConfig("Plain: Harrisburg PA") { customNavigate(Waypoint.builder().setLatLng(40.305301, -76.888941).build(), routesApiPlainToken) },
-      ButtonConfig("No Token: Harrisburg PA") { customNavigate(Waypoint.builder().setLatLng(40.305301, -76.888941).build()) },
+      ButtonConfig("Drive: Harrisburg PA") { customNavigate(Waypoint.builder().setLatLng(40.305301, -76.888941).build(), routesApiPlainToken) },
+      ButtonConfig("Drive: Alford") { customNavigate(Waypoint.builder().setPlaceIdString("ChIJbb5hg6e2j4ARd9VF3w7C87Q").build(), "") },
+      ButtonConfig("showRouteOverview") { withNavigatorAsync {navFragment.showRouteOverview() }},
       ButtonConfig("startGuidance") { withNavigatorAsync {navigator.startGuidance() }},
       ButtonConfig("stopGuidance") { withNavigatorAsync {navigator.stopGuidance() }},
       ButtonConfig("clearDestinations") { withNavigatorAsync {navigator.clearDestinations() }},
